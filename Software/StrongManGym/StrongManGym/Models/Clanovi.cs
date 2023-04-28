f85e6f0844a2a037eaddcf7b3e0dadee915287c4
Nataša Dramac <ndramac21@foi.hr>
2023-04-28 22:50:37 +0200

    Kreirane klase Clanovi, Person i Zaposleni.

---
+++ Software/StrongManGym/StrongManGym/Models/Clanovi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrongManGym.Models
+{
+    public class Clanovi : Person
+    {
+        public int SifraClana { get; set; }
+        public string Email { get; set; }
+        public DateTime DatumRodenja { get; set; }
+        public string Kontakt { get; set; }
+        public string StatusClanarine { get; set; }
+    }
+}
---
+++ Software/StrongManGym/StrongManGym/Models/Zaposleni.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrongManGym.Models
+{
+    public class Zaposleni : Person
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
