d1f4e368fbbb0df913aabee926eb33b72a24d678
NatasaDramac <127131250+NatasaDramac@users.noreply.github.com>
2023-05-05 20:25:11 +0200

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE UlazIzlaz
-(
-    IdUlazIzlaz INT IDENTITY(1,1) PRIMARY KEY,
-    IdClana INT NOT NULL,
-    Ulaz DATETIME NULL,
-    Izlaz DATETIME NULL,
-    CONSTRAINT FK_Clanovi_UlazIzlaz FOREIGN KEY (IdClana) REFERENCES Clanovi(IdClana)
-)
